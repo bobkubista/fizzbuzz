2b3e3b1ee4f92b619a1cb9693945a0b03fb5731e
Bob Kubista <b.kubista@first8.nl>
2020-09-16 18:11:22 +0200

    adding auto formatting

--- src/main/java/com/bobkubista/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/bobkubista/fizzbuzz/FizzBuzz.java
@@ -1,28 +1,27 @@
 package com.bobkubista.fizzbuzz;
 
-
 public class FizzBuzz {
 
-    public String buzz(int i) {
-        String string = "";
-        if (!isFizz(i) && !isBuzz(i)) {
-            return Integer.toString(i);
-        }
-        if (isFizz(i)) {
-            string = "Fizz";
-        }
-        if (isBuzz(i)) {
-            string += "Buzz";
-        }
-        return string;
-    }
+	public String buzz(int i) {
+		String string = "";
+		if (!isFizz(i) && !isBuzz(i)) {
+			return Integer.toString(i);
+		}
+		if (isFizz(i)) {
+			string = "Fizz";
+		}
+		if (isBuzz(i)) {
+			string += "Buzz";
+		}
+		return string;
+	}
 
-    private boolean isBuzz(int i) {
-        return i % 5 == 0;
-    }
+	private boolean isBuzz(int i) {
+		return i % 5 == 0;
+	}
 
-    private boolean isFizz(int i) {
-        return i % 3 == 0;
-    }
+	private boolean isFizz(int i) {
+		return i % 3 == 0;
+	}
 
 }
